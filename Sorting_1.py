868638ffeeb9e1c9d3a6c10c7cc9cfe861d16863
JKB <nonsig@naver.com>
2021-04-16 11:07:08 +0900

    Create Sorting_1.py

--- Sorting_1.py
+++ Sorting_1.py
@@ -2,7 +2,6 @@
 #코딩테스트 K번째 수
 def solution(array, commands):
     answer = []
-    aa=[]
     for i in range(0, len(commands)):
         aa=[]
         for k in range(commands[i][0]-1, commands[i][1]):
@@ -11,3 +10,8 @@ def solution(array, commands):
         answer.append(aa[commands[i][2]-1])
         del(aa)
     return answer
+
+
+#lambda는 함수 선언 대신 사용, map은 list 속 값을 commands로 매핑해줌
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
